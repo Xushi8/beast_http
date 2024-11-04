9284159fc4f27f4cb2a3b6a7b7bb0eeb5a2bfe4c
uuufa <trump@niubi.com>
2024-11-04 17:16:20 +0800

    remove unused libraries

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -1,29 +1,8 @@
 find_package(Threads REQUIRED)
 find_package(OpenMP REQUIRED)
-find_package(TBB CONFIG REQUIRED COMPONENTS tbb)
 find_package(mimalloc CONFIG REQUIRED)
-
-if(BEAST_HTTP_ENABLE_GUI)
-    find_package(Qt6 REQUIRED COMPONENTS Widgets)
-endif()
 
 if(BEAST_HTTP_STATIC_EXTERNAL_LIB)
     set(Boost_USE_STATIC_LIBS ON)
 endif()
 find_package(Boost 1.81.0 REQUIRED)
-
-find_path(RE2_INCLUDE_DIR NAMES re2)
-find_library(RE2_LIB NAMES re2 libre2)
-include_directories(${RE2_INCLUDE_DIR})
-
-find_path(GMP_INCLUDE_DIR NAMES gmp.h)
-find_library(GMP_LIB NAMES gmp libgmp)
-include_directories(${GMP_INCLUDE_DIR})
-
-find_path(MPFR_INCLUDE_DIR NAMES mpfr.h)
-find_library(MPFR_LIB NAMES mpfr libmpfr)
-include_directories(${MPFR_INCLUDE_DIR})
-
-find_path(PQ_INCLUDE_DIR NAMES postgresql)
-find_library(PQ_LIB NAMES pq libpq)
-include_directories(${PQ_INCLUDE_DIR})
